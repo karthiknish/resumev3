00fd7c8ebc9cbacf5426faa7382e05e5cb2ec540
Karthik <karthik.nishanth06@gmail.com>
2024-11-17 19:05:00 +0000

    too many changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,17 @@ module.exports = {
   root: true,
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'simple-import-sort'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false,
+        trailingComma: 'all',
+      },
+    ],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
 }
